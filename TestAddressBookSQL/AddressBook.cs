96d751502288ab6df68d4eb3c7d69e66124b6a46
Saurabh <saurabh.nit20@gmail.com>
2020-11-26 02:42:50 +0530

    [Saurabh].Testing of Addition of Address in API in AddressBook

---
+++ TestAddressBookSQL/AddressBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAddressBookSQL
+{
+    public class AddressBook
+    {
+        public int pin { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+    }
+}
--- TestAddressBookSQL/UnitTest1.cs
+++ TestAddressBookSQL/UnitTest1.cs
@@ -2,30 +2,82 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AddressBookSQL;
 using System.Collections.Generic;
 using System;
+using RestSharp;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace TestAddressBookSQL
 {
     [TestClass]
     public class UnitTest1
     {
+        //Test for Thread
         [TestMethod]
         public void TestMethod1()
         {
-            List<AddressModel> addressModel = new List<AddressModel>();
+            List<AddressModel> addressModel = new List<AddressModel>();                            //List for Multiple List            //
             AddressBookRepo addressBookRepo = new AddressBookRepo();
 
+            //Addition of Multiple Address
             addressModel.Add(new AddressModel("Virat", "Kohli","Captain","Delhi","NewDelhi", "India", "147852","9999999","vk@gmail.com", new DateTime(2020,11,5)));
             addressModel.Add(new AddressModel("Mahi", "Dhoni", "Legend", "Ranchi", "Chennai", "India", "148852", "9989999", "msd@gmail.com", new DateTime(2020, 8, 15)));
 
-            DateTime startDateTime = DateTime.Now;
+
+            //Time taken to Add Contact without Thread
+
+            DateTime startDateTime = DateTime.Now;                                       //Initialization
             addressBookRepo.AddMultipleAddress(addressModel);
-            DateTime endDateTime = DateTime.Now;
+            DateTime endDateTime = DateTime.Now;                                         //Completion
             Console.WriteLine("Without Thread : " + (endDateTime - startDateTime));
 
-            DateTime startDateTimeThread = DateTime.Now;
+
+            //Time taken to Add Contact with Thread
+            
+            DateTime startDateTimeThread = DateTime.Now;                                       //Initialization
             addressBookRepo.AddMultipleAddressWithThread(addressModel);
-            DateTime endDateTimeThread = DateTime.Now;
+            DateTime endDateTimeThread = DateTime.Now;                                         //Completion
             Console.WriteLine("With Thread : " + (endDateTimeThread - startDateTimeThread));
+        }
+
+        RestClient client;
+
+        [TestInitialize]
+
+        //Link to add Link
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:4000/AddressBook");
+        }
+
+
+
+        private IRestResponse getAddressList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/list", Method.GET);              //Retrieving AddressList 
+
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+
+        [TestMethod]
+        public void OnCallingReturnAddressList()
+        {
+            IRestResponse response = getAddressList();
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);                                 //Checking the Status
+            List<AddressBook> dataResponse = JsonConvert.DeserializeObject<List<AddressBook>>(response.Content);          //Desrializing Object
+            Assert.AreEqual(3, dataResponse.Count);                                                          //Counting the responses that matches
+
+
+            //Retrieving the data from the DataResponse 
+            foreach (var item in dataResponse)
+            {
+                Console.WriteLine("Pin Code: " + item.pin + " Name: " + item.name + " City: " + item.city);
+            }
         }
     }
 }
