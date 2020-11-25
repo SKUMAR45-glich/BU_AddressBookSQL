f5d1a4edeafcf86928ff03f89355ce47a3c8ac86
Saurabh <saurabh.nit20@gmail.com>
2020-11-25 21:42:43 +0530

    [Saurabh].Update Data in AddressBook

--- AddressBookSQL/AddressBookRepo.cs
+++ AddressBookSQL/AddressBookRepo.cs
@@ -1,53 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace AddressBookSQL
 {
     public class AddressBookRepo
     {
-        public static string connectionString;
+        public static string connectionString;                                                          
         public static SqlConnection sqlConnection;
 
-        public static List<AddressModel> contacts;
+        public static List<AddressModel> contacts = new List<AddressModel>();                                           //List to AddressModel                           
         public static void SetConnection()
         {
-            connectionString = @"Data Source=DESKTOP-SC0MR56\SQLEXPRESS;Initial Catalog=AddressBookServiceDataBase;Integrated Security=True";
-            sqlConnection = new SqlConnection(connectionString);
+            connectionString = @"Data Source=DESKTOP-SC0MR56\SQLEXPRESS;Initial Catalog=AddressBookServiceDataBase;Integrated Security=True";           //Path for connection
+            sqlConnection = new SqlConnection(connectionString);                                                                                        //Connection Link for Database
         }
-        /// <summary>
-        /// Method to retrieve and display all contacts from addressBook
-        /// hence there are 4 interdependent tables, used joins
-        /// </summary>
-        public static void RetrieveAllContacts()
+
+
+        //Retrieve All ContactDetails
+        public static void RetrieveAllContacts()                                                         
         {
-            contacts = new List<AddressModel>();
+                                                               
             try
             {
-                SetConnection();
+                SetConnection();                                                                                           //Set the connection
                 using (sqlConnection)
                 {
-                    var query = @"select * from AddressBookService";
-                    var sqlCommand = new SqlCommand(query, sqlConnection);
+                    var query = @"select * from AddressBookService";                                                    //Query to select 
+                    var sqlCommand = new SqlCommand(query, sqlConnection);                                              //Command to add query to connection
                     sqlConnection.Open();
-                    var dr = sqlCommand.ExecuteReader();
-                    if (dr.HasRows)
+                    var dr = sqlCommand.ExecuteReader();                                                                //Read the Query
+                    if (dr.HasRows)                                                                                     //Number of rows is >0
                     {
-                        while (dr.Read())
+                        while (dr.Read())                                                                             
                         {
-                            AddressModel addressModel = new AddressModel();
+                            AddressModel addressModel = new AddressModel();                                            //object for AddressModel
+                           
                             addressModel.Id = Convert.ToInt32(dr["Id"]);
                             addressModel.FirstName = dr["FirstName"].ToString();
                             addressModel.LastName = dr["LastName"].ToString();
-                            addressModel.Relation_Type = dr["Relation_Type"].ToString();
                             addressModel.Address = dr["Address"].ToString();
                             addressModel.City = dr["City"].ToString();
                             addressModel.State = dr["State"].ToString();
                             addressModel.Zipcode = dr["ZipCode"].ToString();
                             addressModel.PhoneNumber = dr["PhoneNumber"].ToString();
                             addressModel.Email = dr["Email"].ToString();
-                            contacts.Add(addressModel);
+                            addressModel.Relation_Type = dr["Relation_Type"].ToString();
+
+                            contacts.Add(addressModel);                                               //Add the values to the List
                         }
                     }
 
@@ -60,9 +63,74 @@ namespace AddressBookSQL
             }
             finally
             {
+                sqlConnection.Close();                                                                     //Close the Connection
+            }
+        }
+
+        public static void UpdateContact(AddressModel updateAddressModel)
+        {
+            try
+            {
+                SetConnection();                                                                      //Turn on the connection
+                
+                var sqlCommand = new SqlCommand("SpUpdateCityRelation", sqlConnection);                                 //Stored Procedure     
+                sqlCommand.CommandType = CommandType.StoredProcedure;                                                   //command for Stored Procedure
+                
+
+                sqlCommand.Parameters.AddWithValue("@Id", updateAddressModel.Id);
+                sqlCommand.Parameters.AddWithValue("@FirstName", updateAddressModel.FirstName);
+                sqlCommand.Parameters.AddWithValue("@LastName", updateAddressModel.LastName);
+                sqlCommand.Parameters.AddWithValue("@RelationType", updateAddressModel.Relation_Type);
+                sqlCommand.Parameters.AddWithValue("@Address", updateAddressModel.Address);
+                sqlCommand.Parameters.AddWithValue("@City", updateAddressModel.City);
+                sqlCommand.Parameters.AddWithValue("@State", updateAddressModel.State);
+                sqlCommand.Parameters.AddWithValue("@Zipcode", updateAddressModel.Zipcode);
+                sqlCommand.Parameters.AddWithValue("@PhoneNumber", updateAddressModel.PhoneNumber);
+                sqlCommand.Parameters.AddWithValue("@Email", updateAddressModel.Email);
+                
+                sqlConnection.Open();         
+                
+                var dr = sqlCommand.ExecuteReader();                                                       //Read the Query
+                
+                if (dr.Read())
+                {
+                    
+                    var contactModel = new AddressModel();
+                    contactModel.Id = Convert.ToInt32(dr["Id"]);
+                    contactModel.FirstName = dr["FirstName"].ToString();
+                    contactModel.LastName = dr["LastName"].ToString();
+                    contactModel.Relation_Type = dr["Relation_Type"].ToString();
+                    contactModel.Address = dr["Address"].ToString();
+                    contactModel.City = dr["City"].ToString();
+                    contactModel.State = dr["State"].ToString();
+                    contactModel.Zipcode = dr["ZipCode"].ToString();
+                    contactModel.PhoneNumber = dr["PhoneNumber"].ToString();
+                    contactModel.Email = dr["Email"].ToString();
+
+                    var existingDetails = contacts.FirstOrDefault(c => c.Id == updateAddressModel.Id);          //FInd the ID of contact to update
+                    
+                    if (existingDetails != null)
+                    {
+                        contacts.Remove(existingDetails);                                              //Delete Entire existing
+                        contacts.Add(contactModel);                                                   //Add Completely New
+                    }
+                    else
+                        contacts.Add(contactModel);                                    //No Updation
+                    contactModel.Display();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
                 sqlConnection.Close();
             }
         }
+
+
         public static void DisplayContacts()
         {
             foreach (var contact in contacts)
--- AddressBookSQL/Program.cs
+++ AddressBookSQL/Program.cs
@@ -6,9 +6,18 @@ namespace AddressBookSQL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
-            AddressBookRepo.RetrieveAllContacts();
-            AddressBookRepo.DisplayContacts();
+
+            Console.WriteLine("Welcome to AddressBook");                                     //Welcome Message
+            AddressBookRepo.RetrieveAllContacts();                                           //Retrieve All Contact which is the database
+            AddressBookRepo.DisplayContacts();                                               //Display function to show the Contacts
+
+
+            //To Update the AddressBook
+
+            var contact = AddressBookRepo.contacts[1];                                      //Select the contact to be changed                                            
+            contact.City = "Chennai";                                                       //Enter the city to be changed
+            contact.State = "Tamil Nadu";                                                   //Relocated State
+            AddressBookRepo.UpdateContact(contact);                                         //Update Function the contact
         }
     }
 }
